25f6fe4b51a4bda1376f0bc1c315aeb2c2305aa4
Hora_Est <109119577+s0muchina@users.noreply.github.com>
2024-03-15 16:24:12 -0600

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
