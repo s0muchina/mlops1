59d7a1821e23bf7a66ad6a7a321f88fd1ebc7fda
Hora_Est <109119577+s0muchina@users.noreply.github.com>
2024-03-15 16:30:20 -0600

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
@@ -24,6 +23,8 @@ X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random
 #################################
 
 # Fit a model on the train section
+from sklearn.ensemble import RandomForestRegressor 
+
 regr = RandomForestRegressor(max_depth=2, random_state=seed)
 regr.fit(X_train, y_train)
 
